dee0faac4058f2c7e250b7e2f9692e5edb4f55e1
eltonapsantos10 <67808898+eltonapsantos10@users.noreply.github.com>
2020-11-30 20:53:55 -0300

    simples projeto

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Projects from "../components/projects"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Elton Santos</h1>
+    <p>Web Developer.</p>
+    <p>Meu Site Pessoal.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
